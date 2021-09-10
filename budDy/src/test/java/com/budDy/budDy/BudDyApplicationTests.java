7b2f627950bb40fae42c160f1219783b426590d6
priya-ramesh-s <88005090+priya-ramesh-s@users.noreply.github.com>
2021-09-10 11:26:17 +0100

    Add files via upload

---
+++ budDy/src/test/java/com/budDy/budDy/BudDyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.budDy.budDy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BudDyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
